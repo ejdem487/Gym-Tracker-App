508744bc16816c768addaa8d200cac1d915330ba
Adam <aproch36@gmail.com>
2026-04-17 18:47:42 +0200

    adding for git action test

--- src/main/java/com/ap/gymtracker/GymTrackerApplication.java
+++ src/main/java/com/ap/gymtracker/GymTrackerApplication.java
@@ -2,7 +2,7 @@ package com.ap.gymtracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//adding comment (test for github actions)
 @SpringBootApplication
 public class GymTrackerApplication {
 
